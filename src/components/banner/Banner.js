4c5602e364b614a27181d8384634dcfae5d9d3a9
Felipe Minozzo <edu.minozzo1995@gmail.com>
2023-08-16 19:34:15 -0300

    Add files via upload

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -2,7 +2,7 @@ import "./Banner.css"
 
 export const Banner = () => {
     return(
-        <header>
+        <header className="banner">
             <img src='/images/banner.png' alt="teste pro oregano"/>
         </header>
     )
